8eb73c756e9a9023796cef40f5b796845750f869
xushnud <xushnud.khan@gmail.com>
2021-03-09 20:38:14 +0500

    lasts

--- src/main.js
+++ src/main.js
@@ -3,6 +3,6 @@ import App from './App.vue'
 import router from './router/index'
 import store from './store/index'
 import './assets/style/index.css'
-import VueNumberInput from '@chenfengyuan/vue-number-input';
 
-createApp(App).component(VueNumberInput.name, VueNumberInput).use(router).use(store).mount('#app')
+
+createApp(App).use(router).use(store).mount('#app')
